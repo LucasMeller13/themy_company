05e683ae47205c6f3848ef4c721140a51c1afbcf
Lucas Meller <43608590+LucasMeller13@users.noreply.github.com>
2023-02-26 17:47:48 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -19,7 +19,7 @@ st.sidebar.markdown('## Fastest delivery in town')
 st.sidebar.markdown('''---''')
 
 st.sidebar.markdown('# Selecione uma data limite')
-data = (st.sidebar.slider('Até qual valor?',
+data = (st.sidebar.slider('Até qual data?',
                         min_value=datetime.datetime(2022,2,11),
                         max_value=datetime.datetime(2022,4,6),
                         value=datetime.datetime(2022,3,19),
@@ -33,12 +33,12 @@ st.markdown(
     """
     Growth Dashboard foi construído para acompanhar as métricas de crescimento dos Entregadores e Restaurantes.
     ### Como utilizar esse Growth Dashboard?
-    - Visão Empresa: 
+    - **Visão Empresa:** 
         - Visão Gerencial: Métricas gerais de comportamento.
         - Visão Tática: Indicadores semanais de crescimento.
         - Visão Geográfica: Insights de geolocalização.
-    - Visão Entregador: 
+    - **Visão Entregador:** 
         - Acompanhamento dos indicadores semanais de crescimento
-    - Visão Restaurante:
+    - **Visão Restaurante:**
         - Indicadores semanais de crescimento dos restaurantes
 """ )
